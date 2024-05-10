56eafc09a773d4e2b79002615618896fab048278
Maksim Timofeyev <makstim@list.ru>
2024-05-11 00:03:06 +0300

    Version 1 corrected

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,18 @@ package ru.netology
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val amount: Int = 1000
-    val commission = 0.075
 
-    println("Размер комиссии при сумме перевода $amount рублей: " + (amount * commission) + " рублей")
+    //Мы решили дать пользователям нашей социальной сети возможность отправлять друг другу денежные переводы.
+    // Система будет удерживать комиссию 0.75 %, при этом минимальная комиссия составит 35 рублей.
+    //
+    //Напишите небольшую программу, которая будет высчитывать размер этой комиссии в зависимости от суммы перевода.
+
+    val amount: Int = 100
+    val commission = 0.075
+    val minCommission = 35
+
+    println(
+        "Размер комиссии при сумме перевода $amount рублей: " +
+                (if (amount * commission < minCommission) minCommission else amount * commission) + " рублей"
+    )
 }
